cf117f0cdff24a0902b1c9c84f5c679d3e0e2f8f
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2022-02-24 19:32:02 -0600

    int array sort

--- 345DataStructures/Assignment3_Borchelt/Sorting.java
+++ 345DataStructures/Assignment3_Borchelt/Sorting.java
@@ -45,4 +45,44 @@ public class Sorting {
                     objArray[j+1] = temp;
                 }
     }
+     //https://www.geeksforgeeks.org/selection-sort/
+     public static void selectionSort(int[]  objArray){
+        int n = objArray.length;
+        for (int i = 0; i < n-1; i++){
+            int min_idx = i;
+            for (int j = i+1; j < n; j++){
+                if (objArray[j]<objArray[min_idx])
+                    min_idx = j;
+            }
+            int temp = objArray[min_idx];
+            objArray[min_idx] = objArray[i];
+            objArray[i] = temp;
+        }
+    }
+
+    //https://www.geeksforgeeks.org/insertion-sort/
+    public static void insertionSort(int[] objArray){
+        int n = objArray.length;
+        for (int i = 1; i < n; ++i) {
+            int key = objArray[i];
+            int j = i - 1;
+            while (j >= 0 && objArray[j]>key) {
+                objArray[j + 1] = objArray[j];
+                j = j - 1;
+            }
+            objArray[j + 1] = key;
+        }
+    }
+
+    //https://www.geeksforgeeks.org/selection-sort/
+    public static void bubbleSort(int[] objArray){
+        int n = objArray.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (objArray[j]>objArray[j+1]){
+                    int temp = objArray[j];
+                    objArray[j] = objArray[j+1];
+                    objArray[j+1] = temp;
+                }
+    }
 }
