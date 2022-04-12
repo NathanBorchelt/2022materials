a60cd0521fcfead742fd5bf752e040ed7ce84116
Nathan Borchelt <nathan.borchelt@evsck12.com>
2022-04-12 16:49:06 -0500

    final cc bot

--- 325AdvancedOOP/8Assignment_CookieClicker/CookieClicker.java
+++ 325AdvancedOOP/8Assignment_CookieClicker/CookieClicker.java
@@ -1,3 +1,4 @@
+//Borchelt cookie clicker
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
@@ -10,15 +11,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.awt.event.InputEvent;
 
-//scrapper code from https://www.geeksforgeeks.org/download-web-page-using-java/
-import java.net.URL;
-import java.net.MalformedURLException;
-
 public class CookieClicker {
     public static void main(String[] args)throws IOException, AWTException, InterruptedException, URISyntaxException{
 
         //screen size info
-        //https://www.geeksforgeeks.org/java-program-to-print-screen-resolution/
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int sWidth = (int) screenSize.getWidth();
         int sHeight = (int) screenSize.getHeight();
@@ -28,19 +24,28 @@ public class CookieClicker {
         String website = "https://orteil.dashnet.org/cookieclicker/";
 
 
+        //https://stackoverflow.com/a/5226244
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
             Desktop.getDesktop().browse(new URI(website));
         }
+        else{
+            System.out.println("No web browser to use");
+            System.exit(0);
+        }
 
         System.out.println("5 second sleep to assure page is open and loaded");
+        //what seem to be constant coordinates considering cookie clicker is built of divs with percentages
         int cookieX = (int) (sWidth * 0.16);
         int cookieY = (int) (sHeight * 0.5);
         Thread.sleep(5000);
-        for(int i = 0; i <1000; i++){
+        for(int i = 0; i <10; i++){
             bot.mouseMove(cookieX, cookieY);
             bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            Thread.sleep(2);
+            //14 ms has been the fastest i can run it, 20 allows for vatiation due to other task
+            Thread.sleep(20);
             bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            //5ms delay to assure release of mouse
+            Thread.sleep(5);
             System.out.println("Click #" + (i+1));
         }
 
